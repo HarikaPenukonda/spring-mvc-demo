c2a19390e76d959b1991e76712a607e2677abf33
harika penukonda <penukondaharika@gmail.com>
2020-07-17 16:21:07 -0500

    Create HomeController.java

---
+++ src/com/udemy/springdemo/mvc/HomeController.java
@@ -0,0 +1,19 @@
+package com.udemy.springdemo.mvc;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller // tells spring that this is an mvc controller
+public class HomeController {
+	
+	
+	@RequestMapping("/") /* to map the url to the actual method, 
+							manages all types of http requests for getting posts*/ 
+	public String showPage()
+	{
+		return "main-menu";
+	}
+	
+	
+
+}
