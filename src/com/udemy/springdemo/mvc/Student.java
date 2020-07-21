1df05bff8c3df6675650abdf8a5feba0eecae20d
harika penukonda <penukondaharika@gmail.com>
2020-07-21 11:13:09 -0500

    Update Student.java

--- src/com/udemy/springdemo/mvc/Student.java
+++ src/com/udemy/springdemo/mvc/Student.java
@@ -1,15 +1,28 @@
 package com.udemy.springdemo.mvc;
 
+import java.util.LinkedHashMap;
+
 public class Student {
 	
 	private String firstName;
 	
 	private String lastName;
 	
+	private String country;
+	
+	private LinkedHashMap<String,String> countryOptions;
+	
 	// no-arg constructor
 	public Student()
 	{
-		
+		// populate country options : used ISO country code
+		countryOptions = new LinkedHashMap<>();
+		countryOptions.put("BR", "Brazil");
+		countryOptions.put("FR", "France");
+		countryOptions.put("DE", "Germany");
+		countryOptions.put("IN", "India");
+		countryOptions.put("JP", "Japan");
+		countryOptions.put("US", "United States");
 	}
 
 	public String getFirstName() {
@@ -27,6 +40,22 @@ public class Student {
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public LinkedHashMap<String, String> getCountryOptions() {
+		return countryOptions;
+	}
+	
+	
+	
+	
 	
 	
 
