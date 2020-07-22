16cb8ffa4104d6a820d8d2b7d26e03c420e3f4c8
harika penukonda <penukondaharika@gmail.com>
2020-07-22 10:40:14 -0500

    Register custom editor in controller to avoid whitespace

--- src/com/udemy/springdemo/mvc/CustomerController.java
+++ src/com/udemy/springdemo/mvc/CustomerController.java
@@ -2,15 +2,29 @@ package com.udemy.springdemo.mvc;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
+	
+	// add an initbinder to convert trim input strings
+	// remove leading and trailing whitespace
+	// resolve issue for our validation
+	
+	@InitBinder
+	public void initBinder(WebDataBinder dataBinder) { //initBinder --> will preprocess each web request to our controller
+		StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true); // stringTrimmerEditor --> defined in class SpringAPI,removes leading and trailing whitespace, true --> true
+		dataBinder.registerCustomEditor(String.class,stringTrimmerEditor); // For every String class apply stringTrimmerEditor
+	}
+	
 	
 	@RequestMapping("/showForm")
 	public String showForm(Model theModel) {
